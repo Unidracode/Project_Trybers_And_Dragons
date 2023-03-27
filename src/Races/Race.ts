d848b020797755ab025b4ecce2c52967c08b44ba
Unidracode <unidragorn@gmail.com>
2023-03-27 14:17:30 -0300

    requisito 1

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,20 @@
-//inicia projeto
+export default abstract class Race {
+  constructor(
+    private _name: string, 
+    private _dexterity: number,
+  ) {}
+
+  abstract get maxLifePoints(): number;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
